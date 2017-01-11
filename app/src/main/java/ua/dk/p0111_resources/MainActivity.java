3cfc31f1f3179c8c02f57fa3506fe6576cebcdf7
kazdim <dmytri2009@gmail.com>
2017-01-11 10:27:40 +0200

    Initial commit

---
+++ app/src/main/java/ua/dk/p0111_resources/MainActivity.java
@@ -0,0 +1,17 @@
+package ua.dk.p0111_resources;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        imageView.setImageDrawable(getResources().getDrawable(R.drawable.ic_add_to_photos_black_48dp));
+    }
+}
